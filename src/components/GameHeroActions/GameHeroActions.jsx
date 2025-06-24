901124ca35354dc147e387a879ca51a4e16dd099
Vanderson Alves <vanalvesdev@gmail.com>
2025-06-24 09:58:08 -0300

    add action buttons on mobile view

--- src/components/GameHeroActions/GameHeroActions.jsx
+++ src/components/GameHeroActions/GameHeroActions.jsx
@@ -2,12 +2,16 @@ import './GameHeroActions.css'
 import Icon from '@mdi/react';
 import { mdiChevronDown } from '@mdi/js';
 import { mdiHeartPlus } from '@mdi/js';
+import { mdiCheckCircleOutline } from '@mdi/js';
+import { mdiClockOutline } from '@mdi/js';
+import { mdiThumbDownOutline } from '@mdi/js';
 import { useState } from 'react';
 
 const GameHeroActions = ( { bgColor, isFloating } ) => {
   
   const [chevronAngle, setChevronAngle] = useState(0)
   const [isDropdownActive, setDropdownActive] = useState(false)
+  const [isFloatingButtonActive, setFloatingButtonActive] = useState(false)
 
   const changeDropdownStatus = () => {
     if (chevronAngle == 0){
@@ -18,6 +22,10 @@ const GameHeroActions = ( { bgColor, isFloating } ) => {
       setChevronAngle(0)
       setDropdownActive(false)
     }
+  }
+
+  const handleFloatingButtonClick = () => {
+    setFloatingButtonActive(!isFloatingButtonActive);
   }
 
   return (
@@ -37,12 +45,29 @@ const GameHeroActions = ( { bgColor, isFloating } ) => {
           <a>Action</a>, <a>Fantasy</a>, <a>Historical</a>
         </p>
       </div>
-      { isFloating ? 
-        <div className="floating-button">
-          <button className="button is-primary is-rounded">
-            <Icon path={mdiHeartPlus} size={1.5} />
-          </button>
-        </div> 
+      { isFloating ?
+        <>
+          <div className="floating-button">
+            <button className="button is-primary is-rounded" onClick={handleFloatingButtonClick}>
+              <Icon path={mdiHeartPlus} size={1.2} />
+            </button>
+          </div>
+          <div className={ isFloatingButtonActive ? "dark-film is-active" : "dark-film" }  onClick={handleFloatingButtonClick}/>
+          <div className={ isFloatingButtonActive ? "action-list is-active" : "action-list" }>
+              <button className='action'>
+                <Icon path={mdiCheckCircleOutline} size={2}></Icon>
+                <span>Finished</span>
+              </button>
+              <button className='action'>
+                <Icon path={mdiClockOutline} size={2}></Icon>
+                <span href="#">To Be Played</span>
+              </button>
+              <button className='action'>
+                <Icon path={mdiThumbDownOutline} size={2}></Icon>
+                <span href="#">Dropped</span>
+              </button>
+            </div>
+        </> 
         : 
         <div className={isDropdownActive ? "dropdown is-active" : "dropdown"}>
           <div className="dropdown-trigger">
@@ -55,10 +80,9 @@ const GameHeroActions = ( { bgColor, isFloating } ) => {
           </div>
           <div className="dropdown-menu" id="dropdown-menu" role="menu">
             <div className="dropdown-content">
-              <a href="#" className="dropdown-item">Finished</a>
-              <a className="dropdown-item">To Be Played</a>
-              <a href="#" className="dropdown-item">Dropped</a>
-              <a href="#" className="dropdown-item">Liked</a>
+              <a href="#" className="dropdown-item"><div className="icon"><Icon path={mdiCheckCircleOutline} size={1}></Icon></div>Finished</a>
+              <a href="#" className="dropdown-item"><div className="icon"><Icon path={mdiClockOutline} size={1}></Icon></div>To Be Played</a>
+              <a href="#" className="dropdown-item"><div className="icon"><Icon path={mdiThumbDownOutline} size={1}></Icon></div>Dropped</a>
             </div>
           </div>
         </div> 
